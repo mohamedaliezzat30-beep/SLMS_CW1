87ea2113c772aa6867523426a3c0e860825b8f10
Mohamed Abdelrahman <mohamedaliezzat30@gmail.com>
2026-03-16 10:04:47 +0000

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,29 +1,29 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
 #include "User.h"
+#include <vector>
+#include <string>
 
-User::User() : username(""), password(""), fullName("")
+class Member : public User
 {
-}
+private:
+    std::vector<int> borrowedBookIds;
 
-User::User(const std::string& username, const std::string& password, const std::string& fullName)
-    : username(username), password(password), fullName(fullName)
-{
-}
+public:
+    Member();
+    Member(const std::string& username, const std::string& password, const std::string& fullName);
 
-User::~User()
-{
-}
+    std::string getRole() const override;
 
-std::string User::getUsername() const
-{
-    return username;
-}
+    const std::vector<int>& getBorrowedBookIds() const;
+    bool hasBorrowedBook(int bookId) const;
+    void borrowBook(int bookId);
+    void returnBook(int bookId);
+    void clearBorrowedBooks();
 
-std::string User::getPassword() const
-{
-    return password;
-}
+    std::string serialize() const;
+    static Member deserialize(const std::string& line);
+};
 
-std::string User::getFullName() const
-{
-    return fullName;
-}
+#endif
