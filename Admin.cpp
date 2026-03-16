94297409e0fabfce1eb835e16dec658baf3aaaca
Mohamed Abdelrahman <mohamedaliezzat30@gmail.com>
2026-03-16 07:20:13 +0000

    Upload Admin.cpp code

---
+++ Admin.cpp
@@ -0,0 +1,15 @@
+#include "Admin.h"
+
+Admin::Admin() : User()
+{
+}
+
+Admin::Admin(const std::string& username, const std::string& password, const std::string& fullName)
+    : User(username, password, fullName)
+{
+}
+
+std::string Admin::getRole() const
+{
+    return "Admin";
+}
